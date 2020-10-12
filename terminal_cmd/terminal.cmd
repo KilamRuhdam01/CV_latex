f457effedbccb07bd3c8e623ba41efb610e9b95b
KilamRuhdam1 <eet202757@iitd.ac.in>
2020-10-12 23:55:36 +0530

    CV and Terminal Cmds
    Assignment_1

---
+++ terminal_cmd/terminal.cmd
@@ -0,0 +1,382 @@
+    1  sudo apt install texlive-latex-extra
+    2  sudo apt intall texlive-latex-extra
+    3  sudo apt install texlive-latex-extra
+    4  sudo apt install texstudio
+    5  sudo apt install tikz
+    6  sudo apt install tikzit
+    7  sudo apt install xfig
+    8  sudo apt install gnuplot
+    9  sudo apt install yed
+   10  chmod +x yEd-3.13_*-bit_setup.sh
+   11  wget http://www.yworks.com/products/yed/demo/yEd-3.13_64-bit_setup.sh
+   12  chmod +x yEd-3.13_*-bit_setup.sh
+   13  ./yEd-3.13_*-bit_setup.sh
+   14  kpsewhich etoolbox.sty
+   15  kpsewhich circuitikz.sty
+   16  kpsewhich yed.sty
+   17  kpsewhich gnuplot.sty
+   18  kpsewhich veusz.sty
+   19  kpsewhich xfig.sty
+   20  kpsewhich tikz.sty
+   21  sudo apt-get install -y gnuplot
+   22  sudo apt-get install gnuplot
+   23  sudo apt autoremove libfprint-2-tod1
+   24  kpsewhich gnuplot
+   25  gnuplot
+   26  ./yEd-3.20.1_with-JRE14_64-bit_setup.sh
+   27  ./<yEd-3.20.1_with-JRE14_64-bit_setup.sh>
+   28  /home/madhur/downloads/yEd-3.20.1_with-JRE14_64-bit_setup.sh
+   29  /home/madhur/Downloads/yEd-3.20.1_with-JRE14_64-bit_setup.sh
+   30  chmod +x /home/madhur/Downloads
+   31  /home/madhur/Downloads/yEd-3.20.1_with-JRE14_64-bit_setup.sh
+   32  chmod +x /home/madhur/Downloads/yEd-3.20.1_with-JRE14_64-bit_setup.sh
+   33  /home/madhur/Downloads/yEd-3.20.1_with-JRE14_64-bit_setup.sh
+   34  sudo apt install ubuntu-make
+   35  umake ide eclipse-cpp
+   36  umake ide eclipse
+   37  umake --version
+   38  umake ide--help
+   39  umake ide --help
+   40  umake eclipse-cpp
+   41  umake eclipse -cpp
+   42  umake ide  eclipse -cpp
+   43  umake ide  eclipse-cpp
+   44  umake ide eclipse
+   45  umake ide  eclipse-cpp-
+   46  umake ide --help
+   47  umake ide  eclipse-cpp
+   48  umake ide  eclipse-cpp-
+   49  umake ide  -eclipse-cpp-
+   50  umake --list
+   51  umake ide  -eclipse-jee
+   52  umake ide  eclipse-jee
+   53  umake ide  eclipse-jee-
+   54  umake ide  eclipse-jee
+   55  umake ide eclipse-jee
+   56  sudo apt-get update
+   57  sudo apt-get gdb
+   58  sudo apt-get doxygen
+   59  sudo apt-get install gdb
+   60  sudo apt-get install doxygen
+   61  sudo apt-get install xxgdb
+   62  sudo apt-get install cbeaut
+   63  sudo apt-get install Cbeaut
+   64  sudo apt-get install bcpp
+   65  sudo apt-get install jsbeautifier
+   66  gcc --version
+   67  make --version
+   68  sudo apt-get install patch
+   69  sudo apt-get install diff
+   70  sudo apt-get install git
+   71  sudo apt-get install objcopy
+   72  sudo apt-get install nm
+   73  sudo apt-get install strip
+   74  objdump '/bin/add-apt-repository' 
+   75  objdump --syms  '/bin/add-apt-repository' 
+   76  objdump --syms  /bin/add-apt-repository
+   77  '/bin/activate-global-python-argcomplete3' 
+   78  objcopy '/bin/X11/411toppm' 
+   79  readelf '/bin/X11/411toppm' 
+   80  readelf -I '/bin/X11/411toppm' 
+   81  sudo apt-get MindMap
+   82  sudo apt-get install MindMap
+   83  sudo apt-get install xmind
+   84  sudo apt-get install
+   85  sudo apt-get install '/home/madhur/Downloads/eclipse-inst-linux64.tar.gz' 
+   86  sudo apt install default-jre
+   87  sudo snap install --classic eclipse
+   88  sudo snap install -inkscape
+   89  sudo apt -get install -inkscape
+   90  sudo apt install inkscape
+   91  umake ide eclipse-jee
+   92  umake ide eclipse-cpp
+   93  cp '/home/madhur/Downloads/svg2tikz-master' '/usr/share/inkscape/extensions' 
+   94  sudo -r vym
+   95  sudo apt-get remove eclipse
+   96  dpkg --list
+   97  sudo apt-get autoremove
+   98  sudo apt-get install --eclipse
+   99  sudo apt-get install eclipse
+  100  sudo apt-get install 
+  101  java --version
+  102  sudo apt install default-jre
+  103  sudo apt-get install eclipse-cpp
+  104  umake ide eclipse-cpp
+  105  sudo fdisk -l
+  106  sudo wipefs --all /dev/sdc
+  107  sudo apt-get install texmaker
+  108  sudo apt-get remove texstudio
+  109  ls
+  110  ls -al
+  111  ls -lt
+  112  cd dir downloads
+  113  cd downloads
+  114  cd Downloads
+  115  cd
+  116  pwd
+  117  mkdir sw_lab
+  118  cd sw_dir
+  119  cd sw_lab
+  120  cd
+  121  pwd
+  122  more \Downloads\info
+  123  more \Downloads\intro
+  124  cd sw_lab
+  125  touch new.txt
+  126  more new.txt
+  127  cd Downloads
+  128  cd
+  129  cd Downloads
+  130  head intro.jpeg
+  131  head int.jpeg
+  132  more int.jpeg
+  133  tail int
+  134  tail int.jpeg
+  135  cd
+  136  cd sw_lab
+  137  rm new.txt
+  138  cd
+  139  rm -r sw_lab
+  140  cd sw_lab
+  141  touch int2.jpeg
+  142  cp \Downloads\int.jpeg int2.jpeg
+  143  cd Downloads
+  144  mv int.jpeg int2.jpeg
+  145  mv int2.jpeg int.jpeg
+  146  cd
+  147  ps
+  148  top
+  149  kill pid 2238
+  150  top
+  151  kill pid 2338
+  152  kill 2338
+  153  kill 3937
+  154  KILL 2338
+  155  kill 2338
+  156  top
+  157  kill 2735
+  158  killall top
+  159  kill -l
+  160  pkill -l
+  161  pkill --signal 15
+  162  pkill Document --signal 15
+  163  pgrep Document
+  164  pkill Document
+  165  top
+  166  pkill Xorg
+  167  bg
+  168  fg
+  169  chmod octal sample_report.log
+  170  chmod --help
+  171  chmod octal-4 sample_report.log
+  172  chmod octal-w sample_report.log
+  173  chmod 7 sample_Report.log
+  174  chmod 7 sample_report.log
+  175  grep sample sample_report.txt
+  176  grep sample sample_report.pdf
+  177  date
+  178  time
+  179  uptime
+  180  cal
+  181  w
+  182  whoami
+  183  finger madhur
+  184  sudo apt install finger
+  185  sudo apt-get update finger
+  186  sudo apt-get update 
+  187  sudo apt install finger
+  188  finger madhur
+  189  uname -a
+  190  cat/proc/cpuinfo
+  191  cat
+  192  proc
+  193  cpuinfo
+  194  cpu-info
+  195  cat proc
+  196  man command
+  197  df
+  198  du
+  199  fre
+  200  free
+  201  whereis team
+  202  whereis teams
+  203  whereis latex
+  204  which app
+  205  which teams
+  206  ping 8.8.4.4
+  207  whois lb.com
+  208  sudo apt install whois
+  209  whois lb.com
+  210  dig lb.com
+  211  ping 98.158.52.206
+  212  whois 98.158.52.206
+  213  rpm -qa wget
+  214  yum -y install wget
+  215  apt-get install wget
+  216  sudo apt-get install wget
+  217  wget -c https://cdn.openbsd.org/pub/OpenBSD/
+  218  rm index.html
+  219  exit
+  220  jobs
+  221  kill ping
+  222  pkill ping
+  223  jobs
+  224  pkill ping 8.8.4.4
+  225  pkill -f 'ping 8.8.4.4'
+  226  jobs
+  227  pkill cat
+  228  jobs
+  229  fg
+  230  jobs
+  231  exit
+  232  sudo apt-get install doxyen
+  233  sudo apt-get install doxygen
+  234  whereis doxygen
+  235  ps -a
+  236  ls -l
+  237  ls -lr *
+  238  mkdir sw_lab
+  239  cd sw_lab
+  240  rd sw_lab
+  241  rem sw_lab
+  242  cd
+  243  rmdir sw_lab
+  244  clear
+  245  mkdir sw_lab
+  246  cd sw_lab
+  247  pwd
+  248  touch sample.txt
+  249  chmod 4 sample.txt
+  250  chmod 5 sample.txt
+  251  chmod 500 sample.txt
+  252  wc -w sample.txt
+  253  wc -l sample.txt
+  254  wc|grep the sample.txt
+  255  wc | grep the sample.txt
+  256  grep | wc sample.txt
+  257  grep -o sample.txt | wc
+  258  grep -o sample.txt | wc -l
+  259  grep -o -i  sample.txt | wc -l
+  260  grep the  sample.txt | wc -l
+  261  grep the  sample.txt | wc 
+  262  grep -o -w 'the'  sample.txt | wc -w
+  263  grep -o 'the'  sample.txt | wc -w
+  264  grep -o -w 'the'  sample.txt | wc -w
+  265  grep -o -w -i 'the'  sample.txt | wc -w
+  266  grep  -w -i 'the'  sample.txt | wc -w
+  267  grep -o -w -i 'the'  sample.txt | wc -w
+  268  grep -w -i the sample
+  269  grep -w -i the sample.txt
+  270  grep -o -w -i the sample.txt
+  271  grep -o -w -i the sample.txt| wc 
+  272  grep -o -w -i the sample.txt| wc -l
+  273  grep -o -w  the sample.txt| wc -l
+  274  grep -o  -i the sample.txt| wc -l
+  275  grep -o   the sample.txt| wc -l
+  276  echo "The Earth ech round" >>sample.txt
+  277  chmod 700 sample.txt
+  278  echo "The Earth ech round" >>sample.txt
+  279  date -r sample.txt
+  280  stat -c "%y" sample.txt
+  281  stat -m "%y" sample.txt
+  282  stat -- help
+  283  stat --help
+  284  touch -c sample.txt
+  285  stat -c "%y" sample.txt
+  286  touch sample2.txt
+  287  cp | head sample.txt  sample2.txt
+  288  head sample.txt| echo >>sample2.txt
+  289  head sample.txt  >>sample2.txt
+  290  head -10 sample.txt >> sample2.txt
+  291  head -1 sample.txt >> sample2.txt
+  292  head -10 sample.txt >> sample2.txt
+  293  ln -s /home/Documents/ 
+  294  cd Sample_report
+  295  cd Documents
+  296  history
+  297  cd
+  298  cd sw_lab
+  299  history >>terminal_sw_lab.tt
+  300  history >>terminal_sw_lab.txt
+  301  cd
+  302  history >>terminal_home.txt
+  303  cd sw_lab
+  304  history >>terminal_sw_lab.cmd
+  305  sed 'l s/^/#!/bin/bash\n/' terminal_sw_lab.txt
+  306  sed -i '1 s/^/#!/bin/bash\n/' terminal_sw_lab.txt
+  307  sed -i '1 i/^/#!/bin/bash\n/' terminal_sw_lab.txt
+  308  sed -i '1 i/#!/bin/bash\n/' terminal_sw_lab.txt
+  309  sed -i '1 i #!/bin/bash\n/' terminal_sw_lab.txt
+  310  chmod +x termina_sw_lab.sh
+  311  mv terminal_sw_lab.txt terminal.sh
+  312  chmod +x terminal.sh
+  313  sh terminal.sh
+  314  sudo apt-get install typora
+  315  sudo add-apt-repository 'de https://typora.io/linux ./'
+  316  sudo add-apt-repository 'deb https://typora.io/linux ./'
+  317  sudo -get update
+  318  sudo apt-get update
+  319  sudo apt-get install typora
+  320  snap install typora
+  321  chmod 700 sample_report.log
+  322  chmod 700  /sw_lab/Documents
+  323  cd sw_lab
+  324  ls 
+  325  ls -l
+  326  ls -a
+  327  ln -c /home/madhur/Documents /home/madhur/sw_lab
+  328  ln -s /home/madhur/Documents /home/madhur/sw_lab
+  329  cd Documents
+  330  cd
+  331  cd sw_lab
+  332  history
+  333  cd sw_lab
+  334  hostory
+  335  history
+  336  apt-file -x search '/textcomp.sty$'
+  337  sudo apt install apt-file
+  338  apt-file -x search '/textcomp.sty$'
+  339  apt -file update
+  340  apt-file update
+  341  apt-file -x search '/textcomp.sty$'
+  342  sudo apt-file update
+  343  install -tl -gui
+  344  install-tl -gui
+  345  sudo apt-get install texstudio
+  346  sudo apt-get rem texstudio
+  347  sudo apt rem texstudio
+  348  sudo apt-get remove texstudio
+  349  sudo apt-get install texstudio
+  350  history
+  351  '/home/madhur/Downloads/fig2ps.1.gz' 
+  352  sudo '/home/madhur/Downloads/fig2ps.1.gz' 
+  353  sudo apt-get install -y fig2eps
+  354  sudo apt-get install -y fig2ps
+  355  fig2ps trial1.fig
+  356  inkscape -D image.svg  -o image.pdf --export-latex
+  357  clear
+  358  mkdir sw_lb
+  359  clear
+  360  mkdir sw_lb
+  361  cd sw_lb
+  362  pwd
+  363  touch sample.txt
+  364  chmod 5 sample.txt
+  365  chmod 500 sample.txt
+  366  chmod 600 sample.txt
+  367  chmod 400 sample.txt
+  368  wc -w sample.txt
+  369  grep -o -w -i the sample.txt| wc -l
+  370  grep -o -w -i the sample.txt| wc 
+  371  grep -o -w  the sample.txt| wc -l
+  372  chmod 600 sample.txt
+  373  eco "The Earth ech round" >>sample.txt
+  374  echo "The Earth ech round" >>sample.txt
+  375  date -r sample.txt
+  376  stat -c "%y" sample.txt
+  377  touch -c sample.txt
+  378  stat -c "%y" sample.txt
+  379  touch sample2.txt
+  380  head -10 sample.txt >> sample2.txt
+  381  ln -s /home/Documents/
+  382  history >>terminal.cmd
